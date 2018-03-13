8435a3dc015abd57568b4300a59819f1c09d1f62
bethechr000 <bethechr000@warren.k12.in.us>
2018-03-13 11:17:27 -0400

    The beggining

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,11 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Board extends JPanel {
 
     public Board(){
 
+        setPreferredSize(new Dimension(600, 800));
 
 
     }
--- src/Sprite.java
+++ src/Sprite.java
@@ -1,4 +1,15 @@
-public class Sprite {
+import java.awt.*;
+
+public class Sprite implements Paint, Move {
 
 
+    @Override
+    public void move(int boardHeight, int boardWidth) {
+
+    }
+
+    @Override
+    public Rectangle getBounds() {
+        return null;
+    }
 }
