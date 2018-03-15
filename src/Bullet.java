a55c28231d3b40255cd1e4f423c13fd90537f530
bethechr000 <bethechr000@warren.k12.in.us>
2018-03-15 11:04:01 -0400

    The beggining

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,4 +1,24 @@
-public class Bullet {
+import java.awt.*;
 
+public class Bullet extends Sprite {
+
+
+    public Bullet(Color color, int x, int y, int width, int height) {
+        super(color, x, y, width, height);
+    }
+
+    @Override
+    public void move(int boardHeight, int boardWidth) {
+
+    }
+
+
+    @Override
+    public void paint (Graphics g){
+
+        g.setColor(color);
+        g.fillRect(x, y, width, height);
+
+    }
 
 }
